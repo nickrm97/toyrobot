8d3a4ce30dc205392ed9881fc8d1d33d005589a5
Nicholas Mills <nicholasrmills@icloud.com>
2019-01-29 15:02:22 +1100

    Add basic movement method

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -6,8 +6,45 @@ RSpec.describe Robot do
         expect(Robot.beep).to eq("Beep")
     end 
 
+    it 'Does not initialise if given invalid position' do
+        expect { Robot.new(10, 20, 'FAILME') }.to raise_error(ArgumentError)
+    end
+
     it 'Can report a position' do
         robot = Robot.new(1, 2, 'NORTH')
         expect(robot.report).to eq("I am at X: 1, Y: 2 facing NORTH")
+    end
+
+    it 'Can move forward one space SOUTH' do
+        robot = Robot.new(1, 2, 'NORTH')
+        robot.move
+        expect(robot.report).to eq("I am at X: 1, Y: 3 facing NORTH")    
+    end
+
+    it 'Does not move off board' do
+        robot = Robot.new(1, 4, 'NORTH')
+        expect(robot.move).to eq("Invalid move")    
+    end
+
+    it 'Can move forward one space SOUTH' do
+        robot = Robot.new(1, 2, 'SOUTH')
+        robot.move
+        expect(robot.report).to eq("I am at X: 1, Y: 1 facing SOUTH")    
+    end
+
+    it 'Can move forward one space EAST' do
+        robot = Robot.new(1, 2, 'EAST')
+        robot.move
+        expect(robot.report).to eq("I am at X: 2, Y: 2 facing EAST")    
+    end
+
+    it 'Can move forward one space WEST' do
+        robot = Robot.new(1, 2, 'WEST')
+        robot.move
+        expect(robot.report).to eq("I am at X: 0, Y: 2 facing WEST")    
+    end
+
+    it 'Can turn left correctly' do
+        pending
     end
 end
--- src/robot.rb
+++ src/robot.rb
@@ -1,4 +1,8 @@
 require 'pry'
+
+DIRECTIONS = ["NORTH", "SOUTH", "EAST", "WEST"]
+
+
 class Robot
     attr_reader :x, :y, :f
     def self.beep
@@ -6,12 +10,30 @@ class Robot
     end
 
     def initialize(x, y, f)
-        @x = x
-        @y = y
-        @f = f
+        if (x.between?(0,5) && y.between?(0,5) && DIRECTIONS.include?(f) )
+            @x = x
+            @y = y
+            @f = f
+        else
+            raise ArgumentError, "Invaid coordinate inputs"
+        end
     end
 
     def report
         "I am at X: #{x}, Y: #{y} facing #{f}"
     end
+
+    def move
+        if @f == 'NORTH' && @y < 4
+            @y += 1
+        elsif @f == 'SOUTH' && @y > 0
+            @y -= 1
+        elsif @f == 'EAST' && @x < 4
+            @x += 1
+        elsif @f == 'WEST' && @x > 0
+            @x -= 1
+        else
+            "Invalid move"
+        end
+    end     
 end
