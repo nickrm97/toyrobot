44de76a9fd70f28446de0773da7a6ef98be2dc52
Nicholas Mills <nicholasrmills@icloud.com>
2019-01-29 12:26:41 +1100

    Add report method to robot class

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Robot do
     it "can beep" do
         expect(Robot.beep).to eq("Beep")
     end 
+
+    it 'Can report a position' do
+        robot = Robot.new(1, 2, 'NORTH')
+        expect(robot.report).to eq("I am at X: 1, Y: 2 facing NORTH")
+    end
 end
--- src/robot.rb
+++ src/robot.rb
@@ -1,5 +1,17 @@
+require 'pry'
 class Robot
+    attr_reader :x, :y, :f
     def self.beep
         "Beep"
+    end
+
+    def initialize(x, y, f)
+        @x = x
+        @y = y
+        @f = f
+    end
+
+    def report
+        "I am at X: #{x}, Y: #{y} facing #{f}"
     end
 end
